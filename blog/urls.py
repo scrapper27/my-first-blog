12c25bf95b02ebe1ab367a538ae7f07161b41752
scrapper27 <stackhouseowen606@gmail.com>
2023-12-12 00:02:20 -0500

    first commit

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+
 ]
